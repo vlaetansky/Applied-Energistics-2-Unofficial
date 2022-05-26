c8a541e3cb394de7b9a1a86565e2a075a6439f90
GlodBlock <1356392126@qq.com>
2022-05-26 22:33:59 +0800

    cal expression in cca bar

---
+++ src/main/java/appeng/util/calculators/ArithHelper.java
@@ -0,0 +1,136 @@
+package appeng.util.calculators;
+
+public class ArithHelper {
+
+    private static final int DEF_DIV_SCALE = 16;
+
+    private ArithHelper() {
+    }
+
+    /**
+     * addition
+     *
+     * @param v1 p1
+     * @param v2 p2
+     * @return sum
+     */
+
+    public static double add(double v1, double v2) {
+        java.math.BigDecimal b1 = new java.math.BigDecimal(Double.toString(v1));
+        java.math.BigDecimal b2 = new java.math.BigDecimal(Double.toString(v2));
+        return b1.add(b2).doubleValue();
+    }
+
+    public static double add(String v1, String v2) {
+        java.math.BigDecimal b1 = new java.math.BigDecimal(v1);
+        java.math.BigDecimal b2 = new java.math.BigDecimal(v2);
+        return b1.add(b2).doubleValue();
+    }
+
+    /**
+     * subtraction
+     *
+     * @param v1 p1
+     * @param v2 p2
+     * @return sub
+     */
+
+    public static double sub(double v1, double v2) {
+        java.math.BigDecimal b1 = new java.math.BigDecimal(Double.toString(v1));
+        java.math.BigDecimal b2 = new java.math.BigDecimal(Double.toString(v2));
+        return b1.subtract(b2).doubleValue();
+    }
+
+    public static double sub(String v1, String v2) {
+        java.math.BigDecimal b1 = new java.math.BigDecimal(v1);
+        java.math.BigDecimal b2 = new java.math.BigDecimal(v2);
+        return b1.subtract(b2).doubleValue();
+    }
+
+    /**
+     * multiplication
+     *
+     * @param v1
+     *            p1
+     * @param v2
+     *            p2
+     * @return mul
+     */
+
+    public static double mul(double v1, double v2) {
+        java.math.BigDecimal b1 = new java.math.BigDecimal(Double.toString(v1));
+        java.math.BigDecimal b2 = new java.math.BigDecimal(Double.toString(v2));
+        return b1.multiply(b2).doubleValue();
+    }
+
+    public static double mul(String v1, String v2) {
+        java.math.BigDecimal b1 = new java.math.BigDecimal(v1);
+        java.math.BigDecimal b2 = new java.math.BigDecimal(v2);
+        return b1.multiply(b2).doubleValue();
+    }
+
+    /**
+     * division. e = 10^-10
+     *
+     * @param v1
+     *            p1
+     * @param v2
+     *            p2
+     * @return div
+     */
+
+    public static double div(double v1, double v2) {
+        return div(v1, v2, DEF_DIV_SCALE);
+    }
+
+    public static double div(String v1, String v2) {
+        java.math.BigDecimal b1 = new java.math.BigDecimal(v1);
+        java.math.BigDecimal b2 = new java.math.BigDecimal(v2);
+        return b1.divide(b2, DEF_DIV_SCALE, java.math.BigDecimal.ROUND_HALF_UP).doubleValue();
+    }
+
+    /**
+     * division. e = 10^-scale
+     *
+     * @param v1 p1
+     * @param v2 p2
+     * @param scale scale
+     * @return div
+     */
+
+    public static double div(double v1, double v2, int scale) {
+        if (scale < 0) {
+            throw new IllegalArgumentException("The scale must be a positive integer or zero");
+        }
+        java.math.BigDecimal b1 = new java.math.BigDecimal(Double.toString(v1));
+        java.math.BigDecimal b2 = new java.math.BigDecimal(Double.toString(v2));
+        return b1.divide(b2, scale, java.math.BigDecimal.ROUND_HALF_UP).doubleValue();
+    }
+
+    /**
+     * rounding
+     *
+     * @param v p
+     * @param scale scale
+     * @return result
+     */
+
+    public static double round(double v, int scale) {
+        if (scale < 0) {
+            throw new IllegalArgumentException("The scale must be a positive integer or zero");
+        }
+        java.math.BigDecimal b = new java.math.BigDecimal(Double.toString(v));
+        java.math.BigDecimal one = new java.math.BigDecimal("1");
+        return b.divide(one, scale, java.math.BigDecimal.ROUND_HALF_UP).doubleValue();
+    }
+
+    public static double round(String v, int scale) {
+        if (scale < 0) {
+            throw new IllegalArgumentException("The scale must be a positive integer or zero");
+        }
+        java.math.BigDecimal b = new java.math.BigDecimal(v);
+        java.math.BigDecimal one = new java.math.BigDecimal("1");
+        return b.divide(one, scale, java.math.BigDecimal.ROUND_HALF_UP).doubleValue();
+    }
+
+}
---
+++ src/main/java/appeng/util/calculators/Calculator.java
@@ -0,0 +1,190 @@
+package appeng.util.calculators;
+
+import java.util.Collections;
+import java.util.Stack;
+
+public class Calculator {
+    private final Stack<String> postfixStack = new Stack<>();
+    private final Stack<Character> opStack = new Stack<>();
+    private final int[] operatPriority = new int[] { 0, 3, 2, 1, -1, 1, 0, 2 };
+
+    public static double conversion(String expression) {
+        double result = 0;
+
+        if( expression == null )
+            return Double.NaN;
+
+        expression = expression.replace( " ", "" );
+
+        Calculator cal = new Calculator();
+        if( expression.length() == 1 && Character.isDigit( expression.charAt( 0 ) ) ) {
+            return expression.charAt( 0 ) - '0';
+        }
+        try {
+            expression = transform(expression);
+            result = cal.calculate(expression);
+        } catch (Exception e) {
+            return Double.NaN;
+        }
+        // return new String().valueOf(result);
+        return result;
+    }
+
+    /**
+     * replace '-' with '~'
+     *
+     * @param expression
+     *            e.g.-2+-1*(-3E-2)-(-1) -> ~2+~1*(~3E~2)-(~1)
+     * @return
+     */
+    private static String transform(String expression) {
+        char[] arr = expression.toCharArray();
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] == '-') {
+                if (i == 0) {
+                    arr[i] = '~';
+                } else {
+                    char c = arr[i - 1];
+                    if (c == '+' || c == '-' || c == '*' || c == '/' || c == '(' || c == 'E' || c == 'e') {
+                        arr[i] = '~';
+                    }
+                }
+            }
+        }
+        if(arr[0]=='~'||arr[1]=='(') {
+            arr[0]='-';
+            return "0" + new String(arr);
+        } else {
+            return new String(arr);
+        }
+    }
+
+    /**
+     * Do calculation
+     *
+     * @param expression
+     *            e.g.5+12*(3+5)/7
+     * @return
+     */
+    public double calculate(String expression) {
+        Stack<String> resultStack = new Stack<>();
+        prepare(expression);
+        Collections.reverse(postfixStack);
+        String firstValue, secondValue, currentValue;
+        while (!postfixStack.isEmpty()) {
+            currentValue = postfixStack.pop();
+            if (!isOperator(currentValue.charAt(0))) {
+                currentValue = currentValue.replace("~", "-");
+                resultStack.push(currentValue);
+            } else {
+                secondValue = resultStack.pop();
+                firstValue = resultStack.pop();
+
+                firstValue = firstValue.replace("~", "-");
+                secondValue = secondValue.replace("~", "-");
+
+                String tempResult = calculate(firstValue, secondValue, currentValue.charAt(0));
+                resultStack.push(tempResult);
+            }
+        }
+        return Double.parseDouble(resultStack.pop());
+    }
+
+    /**
+     * Turn expression into postfix stack
+     *
+     * @param expression
+     */
+    private void prepare(String expression) {
+        opStack.push(',');
+        char[] arr = expression.toCharArray();
+        int currentIndex = 0;
+        int count = 0;
+        char currentOp, peekOp;
+        for (int i = 0; i < arr.length; i++) {
+            currentOp = arr[i];
+            if (isOperator(currentOp)) {
+                if (count > 0) {
+                    postfixStack.push(new String(arr, currentIndex, count));
+                }
+                peekOp = opStack.peek();
+                if (currentOp == ')') {
+                    while (opStack.peek() != '(') {
+                        postfixStack.push(String.valueOf(opStack.pop()));
+                    }
+                    opStack.pop();
+                } else {
+                    while (currentOp != '(' && peekOp != ',' && compare(currentOp, peekOp)) {
+                        postfixStack.push(String.valueOf(opStack.pop()));
+                        peekOp = opStack.peek();
+                    }
+                    opStack.push(currentOp);
+                }
+                count = 0;
+                currentIndex = i + 1;
+            } else {
+                count++;
+            }
+        }
+        if (count > 1 || (count == 1 && !isOperator(arr[currentIndex]))) {
+            postfixStack.push(new String(arr, currentIndex, count));
+        }
+
+        while (opStack.peek() != ',') {
+            postfixStack.push(String.valueOf(opStack.pop()));
+        }
+    }
+
+    /**
+     * Check is it a valid operator
+     *
+     * @param c
+     * @return
+     */
+    public static boolean isOperator(char c) {
+        return c == '+' || c == '-' || c == '*' || c == '/' || c == '(' || c == ')';
+    }
+
+    /**
+     * priority
+     *
+     * @param cur
+     * @param peek
+     * @return
+     */
+    public boolean compare(char cur, char peek) {
+        boolean result = false;
+        if (operatPriority[(peek) - 40] >= operatPriority[(cur) - 40]) {
+            result = true;
+        }
+        return result;
+    }
+
+    /**
+     * Do calculator with operater
+     *
+     * @param firstValue
+     * @param secondValue
+     * @param currentOp
+     * @return
+     */
+    private String calculate(String firstValue, String secondValue, char currentOp) {
+        String result = "";
+        switch (currentOp) {
+            case '+':
+                result = String.valueOf(ArithHelper.add(firstValue, secondValue));
+                break;
+            case '-':
+                result = String.valueOf(ArithHelper.sub(firstValue, secondValue));
+                break;
+            case '*':
+                result = String.valueOf(ArithHelper.mul(firstValue, secondValue));
+                break;
+            case '/':
+                result = String.valueOf(ArithHelper.div(firstValue, secondValue));
+                break;
+        }
+        return result;
+    }
+
+}
